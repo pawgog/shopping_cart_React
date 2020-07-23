034e1bc8557292a476b812da2fec2ffa415b323c
pawgog <p.gogola@interia.pl>
2020-07-23 22:12:55 +0200

    Set up react app.

---
+++ src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Dashboard extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Test</h1>
+      </>
+    );
+  }
+}
+
+export default Dashboard;
